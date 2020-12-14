bacd36040c9453caddae2fad6e9841eabae05cbc
joaohenrique87 <74745229+joaohenrique87@users.noreply.github.com>
2020-12-13 21:40:58 -0300

    Extrator parte 1

---
+++ Candidato.py
@@ -0,0 +1,22 @@
+
+import mysql.connector
+con = mysql.connector.connect(host='localhost',database='Gerenciamento_PSS',user='root',password='')
+if con.is_connected():
+    db_info = con.get_server_info()
+    print("Conectado ao servidor MySQL versão ",db_info)
+    cursor = con.cursor()
+    linha = cursor.fetchone()
+    print("Conectado ao banco de dados ")
+
+mycursor = con.cursor()
+
+mycursor.execute ("Select * from Candidato")
+
+
+
+
+myresult = mycursor.fetchall()
+
+
+for x in myresult:
+    print( "Os Dados encontrados foram: ")
